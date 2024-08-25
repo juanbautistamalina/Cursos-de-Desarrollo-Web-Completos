1b2aae99cbc505fd268ee7aa1ea92fda53b346a0
Juan Bautista Malina <malinajuanbautista@gmail.com>
2024-08-25 18:06:49 -0300

    location

---
+++ JavaScript/2) MID Level/location/script.js
@@ -0,0 +1,13 @@
+const href = window.location.href;
+document.write(href + "<br>");
+
+const hostname = window.location.hostname;
+document.write(hostname + "<br>");
+
+const pathname = window.location.pathname;
+document.write(pathname + "<br>");
+
+const protocol = window.location.protocol;
+document.write(protocol + "<br>")
+
+const assing = window.location.assign("https://youtube.com");
