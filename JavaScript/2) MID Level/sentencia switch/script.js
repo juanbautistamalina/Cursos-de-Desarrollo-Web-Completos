03c3e6b5d10d1cb091dab566001c354e0319456c
Juan Bautista Malina <malinajuanbautista@gmail.com>
2024-08-26 18:13:15 -0300

    sentencia switch

---
+++ JavaScript/2) MID Level/sentencia switch/script.js
@@ -0,0 +1,27 @@
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Hoy es lunes");
+        break;
+    case 2:
+        console.log("Hoy es martes");
+        break;
+    case 3:
+        console.log("Hoy es miércoles");
+        break;
+    case 4:
+        console.log("Hoy es jueves");
+        break;
+    case 5:
+        console.log("Hoy es viernes");
+        break;
+    case 6:
+        console.log("Hoy es sábado");
+        break;
+    case 7:
+        console.log("Hoy es domingo");
+        break;
+    default:
+        console.log("Día no válido");
+}
